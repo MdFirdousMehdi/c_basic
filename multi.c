0fb6808fa2f3b537808c97a4b61658ad5387eec2
SARATH.S <sarathsivan099@gmail.com>
2020-10-05 15:02:56 +0530

    Update multi.c

--- multi.c
+++ multi.c
@@ -19,11 +19,3 @@ int main()
 
 }
 			
-/* OUTPUT:
-
-Enter the number:
-1                                                           1                                                            Multiplication table                                       1 * 1= 1                                                    2 * 1= 2                                                    3 * 1= 3                                                    4 * 1= 4                                                    5 * 1= 5                                                    6 * 1= 6                                                    7 * 1= 7                                                    8 * 1= 8
-9 * 1= 9
-10 * 1= 10
-
-*/
